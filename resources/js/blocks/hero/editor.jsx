e49496aeda6e33624d9ca19fea4e464280a669b2
Jasper Frumau <jasper@imagewize.com>
2025-05-07 17:09:04 +0700

    block margins

--- resources/js/blocks/hero/editor.jsx
+++ resources/js/blocks/hero/editor.jsx
@@ -20,10 +20,24 @@ export default function Edit({ attributes, setAttributes }) {
   const TEMPLATE = [
     ['core/spacer', { height: '100px', width: '0px' }],
     ['core/group', { 
-      layout: { type: 'flex', flexWrap: 'nowrap', justifyContent: 'center' }
+      layout: { type: 'flex', flexWrap: 'nowrap', justifyContent: 'center' },
+      style: { 
+        spacing: {
+          padding: {
+            left: "var:preset|spacing|50",
+            right: "var:preset|spacing|50"
+          }
+        }
+      }
     }, [
       ['core/group', { 
-        layout: { type: 'flex', orientation: 'vertical' }
+        layout: { type: 'flex', orientation: 'vertical' },
+        style: { 
+          spacing: { margin: { left: 'auto', right: 'auto' } }
+        },
+        // Set a max width for better readability
+        backgroundColor: "transparent",
+        contentSize: "800px",
       }, [
         ['core/heading', { 
           placeholder: 'Add main heading here...',
@@ -37,7 +51,13 @@ export default function Edit({ attributes, setAttributes }) {
           style: { typography: { fontWeight: '100' } }
         }]
       ]],
-      ['core/image', { aspectRatio: '16/9', scale: 'cover' }],
+      ['core/image', { 
+        aspectRatio: '16/9', 
+        scale: 'cover',
+        style: { 
+          spacing: { margin: { left: 'auto', right: 'auto' } }
+        }
+      }],
       ['core/spacer', { height: '100px', width: '0px' }]
     ]],
     ['core/spacer', { height: '100px', width: '0px' }]
