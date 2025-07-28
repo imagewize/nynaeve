d312f75f46e84595adb0994387226646e55b60ed
Jasper Frumau <jasper@imagewize.com>
2025-07-28 10:15:28 +0700

    Related Articles Improved Filtering & Styling

--- resources/js/blocks/related-articles/save.jsx
+++ resources/js/blocks/related-articles/save.jsx
@@ -8,14 +8,25 @@ import { useBlockProps } from '@wordpress/block-editor';
  * The actual content is rendered dynamically by view.js
  */
 export default function Save({ attributes }) {
-  const { title, numberOfPosts, relatedBy } = attributes;
+  const { title, numberOfPosts, relatedBy, maxCommonThreshold, maxTagsToUse, titleAlignment, headerLevel } = attributes;
   const blockProps = useBlockProps.save({
     className: 'wp-block-imagewize-related-articles',
   });
   
   return (
-    <div { ...blockProps } data-number-of-posts={numberOfPosts} data-related-by={relatedBy}>
-      <h2 className="related-articles-title">{title}</h2>
+    <div { ...blockProps } 
+         data-number-of-posts={numberOfPosts} 
+         data-related-by={relatedBy}
+         {...(maxCommonThreshold !== 50 && { 'data-max-common-threshold': maxCommonThreshold })}
+         {...(maxTagsToUse !== 3 && { 'data-max-tags-to-use': maxTagsToUse })}>
+      {React.createElement(
+        `h${headerLevel}`,
+        {
+          className: 'related-articles-title',
+          style: { textAlign: titleAlignment }
+        },
+        title
+      )}
       <div className="related-articles-grid" id="related-articles-container">
         {/* Content will be loaded by view.js */}
         <div className="related-articles-loading">
